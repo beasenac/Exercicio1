ccc265095c10d57acf036aa97939df4c9ecbf314
dyllavic <dyllavick@gmail.com>
2023-09-25 18:07:34 -0300

    Add project files.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+int numero1,numero2,numero3,maiorN,menorN;
+Console.WriteLine("Digite o primeiro números:");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número:");
+numero3 = Convert.ToInt32(Console.ReadLine());
+
+if (numero1 > numero2 && numero1 > numero3 )
+{
+  
+    Console.WriteLine("O maior número é:" + numero1);
+}
+else if(numero2 > numero3)
+{
+    Console.WriteLine("O maior número é:" + numero2);
+}
+else
+{
+    Console.WriteLine("O maior número é :" + numero3);
+}
+if (numero1 < numero2 && numero1 < numero3)
+{
+
+    Console.WriteLine("O menor número é:" + numero1);
+}
+else if (numero2 < numero3)
+{
+    Console.WriteLine("O menor número é:" + numero2);
+}
+else
+{
+    Console.WriteLine("O menor número é:" + numero3);
+}
+
+
+
+
+
